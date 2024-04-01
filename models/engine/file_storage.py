8638a59c982cc125f7bfd50f00d9a0955eb1b61a
daveyhmariam <daveyhmariam@gmail.com>
2024-04-01 20:55:07 +0300

    count method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        '''retreiving a single item for db'''
+        objs = self.all(cls)
+        for obj in objs.values():
+            if id == obj.id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        '''counting objects of same type or all'''
+        return len(self.all(cls))
