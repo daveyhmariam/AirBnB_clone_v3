1c7a008c9dac8a4c2e0f36c3ab3e1d7e4d06c530
daveyhmariam <daveyhmariam@gmail.com>
2024-04-01 21:04:29 +0300

    api stat

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This Python script sets up a Flask web application with
+error handling and a teardown function for
+closing the database connection.
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_view
+import os
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_view)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    '''close database connection'''
+    storage.close()
+
+
+@app.errorhandler(404)
+def handler(e):
+    '''404 handler'''
+    return jsonify({'error': 'Not found'}), 404  # response
+
+
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST')
+    port = os.getenv('HBNB_API_PORT')
+
+    host = host if host else '0.0.0.0'
+    port = int(port) if port else 5000
+    app.run(host=host, port=port, threaded=True)
