70856b7c9d05621b7c096fa2982d16d62d1102d0
Danielyilma <yilmad752@gmail.com>
2024-04-02 04:44:35 +0300

    cross origin resource sharing

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,13 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_view
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_view)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
